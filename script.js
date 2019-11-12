cb207405c3768849b27315c9b8919df43ec45d2f
Brandon Sin <46876645+BrandonSin@users.noreply.github.com>
2019-11-11 23:35:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//script
+function gameReview(){
+    window.open("gameReview.html");
+}
+
+function resume(){
+    window.open("docs/Brandon Sin - Resume.pdf");
+}
+
+function linkedIn(){
+    window.open("https://www.linkedin.com/in/brandon-sin-28782ba8/");
+}
+
+function facebook(){
+    window.open("https://www.facebook.com/brandon.sin.712");
+}
+
+
+
+
